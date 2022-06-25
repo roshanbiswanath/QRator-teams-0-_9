cac0387a944fe0287014dc21cec45b481a7ecd4b
P.Biswanath Patra <63574390+roshanbiswanath@users.noreply.github.com>
2022-06-26 01:50:07 +0530

    Update main.py

--- ScanEr/main.py
+++ ScanEr/main.py
@@ -1,14 +1,9 @@
-#!/usr/bin/env python
-from time import time
+import time
 import PySimpleGUI as sg
 import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
 import time
-
-"""
-Demo program that displays a webcam using OpenCV
-"""
 
 
 def main():
@@ -17,12 +12,12 @@ def main():
 
     # define the window layout
     layout = [[sg.Text('QRator', size=(40, 1), justification='center', font='Helvetica 20')],
-              [sg.Image(filename='', key='image')],
+              [sg.Image(filename='',size = (40,40), key='image')],
               [
                sg.Text('Scanning...', size=(10, 1), font='Helvetica 14',key='status'), ]]
 
     # create the window and show it without the plot
-    window = sg.Window('Demo Application - OpenCV Integration',
+    window = sg.Window('QRator - QR Scanner',
                        layout, location=(800, 400))
 
     # ---===--- Event LOOP Read and display frames, operate the GUI --- #
@@ -36,15 +31,24 @@ def main():
         if verify(qr):
             window.Element('status').update('Verified')
             
+            window['image'].update('rename.gif',size=(650,300))
+            window.refresh()
+            t1 = time.time()
+            while True:
+                t2 = time.time()
+                if (t2 - t1) > 4:
+                    window.Element('status').update('Scanning...')
+                    break
         print(qr)
 
     def decoder(img):
         gray = cv2.cvtColor(img,0)
         qr = decode(gray)
+
         return qr
     while True:
         
-        event, values = window.read(timeout=10)
+        event, values = window.read(timeout=15)
 
         if event == 'Exit' or event == sg.WIN_CLOSED:
             return
@@ -56,6 +60,7 @@ def main():
                 pass
             else:
                 qrfound(qr[0].data.decode('utf-8'))
+            
             imgbytes = cv2.imencode('.png', frame)[1].tobytes()  # ditto
             window['image'].update(data=imgbytes)
 
